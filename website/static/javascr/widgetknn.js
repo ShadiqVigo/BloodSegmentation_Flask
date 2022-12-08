57d6259fdd31f70e96b23f0bb54f0863cb578996
ShadiqVigo <92839336+ShadiqVigo@users.noreply.github.com>
2022-12-08 22:41:24 +0700

    Add files via upload

---
+++ website/static/javascr/widgetknn.js
@@ -0,0 +1,21 @@
+function pee(){
+    document.getElementById('myImage')
+    .src="static/images/lungicon.png";
+    document.getElementById('massage')
+    /*.innerHTML="<h1> Pulmonary Embolism </h1> <p> Pulmonary embolism is a blockage in one of the pulmonary arteries <br> in lung. In most cases, pulmonary embolism is caused by blood clots <br> that travel to the lungs from deep veins in the legs or from veins in <br> other parts of the body. <br><br> <font size=+1;> <b> Common Signs and Symptoms </b> </font> : <br> <b>Shortness of breath</b>. This symptom typically appears suddenly <br> and always gets worse with exertion. <br><br> <b> Chest pain </b>. Pain is often sharp and felt when you breathe in deeply. <br> It can also be felt when you cough, bend or stoop. <br><br> <b> Cough </b>. May produce bloody or blood-streaked sputum. </p>" ;*/
+}
+
+function dvtee(){
+    document.getElementById('myImage')
+    .src="static/images/kakicon.png";
+    document.getElementById('massage')
+    /*.innerHTML="<h1> Deep Vein Thrombosis </h1>  <p> Deep Vein Thrombosis occurs when a blood clot forms in one or <br> more of the deep veins in the body, usually in the legs. Blood clot <br> in the legs can also develop no movement for a long time. Blood <br> clots in the veins can break loose and then travel through the <br> bloodstream and get stuck in the lungs.<br><br> <font size=+1;> <b> Common Signs and Symptoms </b> </font> : <br> <b> Leg swelling </b> <br><br> <b> Leg Pain. </b> Cramping or soreness often starts in the calf. <br><br> <b> Change in skin color. </b> Such as red or purple, depending on the <br> color of skin <br><br> <b> Feeling of warmth on the affected leg </b> </p>";*/
+}
+
+function svte(){
+    document.getElementById('myImage')
+    .src="static/images/hearticon.png";
+    document.getElementById('massage')
+    /*.innerHTML="<h1> Superficial Venous <br> Thrombosis </h1>  <p> Superficial venous thrombosis is a blood clot in a superficial vein <br> of the upper or lower extremities or less commonly, in one or more <br> veins of the chest or breast.  Superficial Venous Thrombosis in the <br> upper extremity most commonly results from IV infusions. <br><br> <font size=+1;> <b> Common Signs and Symptoms </b> </font> : <br> Signs of superficial venous thrombosis are similar to DVT: <br> <b> Redness and swelling </b> <br> <b> Feeling of warmth </b> <br> <b> Tenderness </b> <br> <b> Pain </b> </p>";*/
+}
+ 
